32ebf266dd64f8cb8b1e5e4dda411d8b86f8be0a
Mike Willey <mikewilley@gmail.com>
2026-01-08 13:23:05 -0500

    added correct numbers

--- app/components/TeamRoster.tsx
+++ app/components/TeamRoster.tsx
@@ -13,20 +13,20 @@ const COACHES = {
 };
 
 const PLAYERS: Player[] = [
-  { id: 1, number: 23, name: "Adelaide Kandl", grade: 8 },
-  { id: 2, number: 15, name: "Aria McLaurin", grade: 8 },
-  { id: 3, number: 32, name: "Vienna Thomson", grade: 8 },
+  { id: 1, number: 1, name: "Adelaide Kandl", grade: 8 },
+  { id: 2, number: 25, name: "Aria McLaurin", grade: 8 },
+  { id: 3, number: 21, name: "Vienna Thomson", grade: 8 },
 
-  { id: 4, number: 4, name: "Evie Bagnell", grade: 7 },
-  { id: 5, number: 12, name: "Charlotte Hogge", grade: 7 },
-  { id: 6, number: 7, name: "Pearl Barry", grade: 7 },
-  { id: 7, number: 21, name: "Devan Bryant", grade: 7 },
-  { id: 8, number: 10, name: "Ellie Montgomery", grade: 7 },
-  { id: 9, number: 5, name: "Harper Roberts", grade: 7 },
-  { id: 10, number: 18, name: "Zinn Scalin", grade: 7 },
-  { id: 11, number: 14, name: "Louise Totton", grade: 7 },
-  { id: 12, number: 3, name: "Mary Willey", grade: 7 },
-  { id: 13, number: 11, name: "Jaidah Williams", grade: 7 },
+  { id: 4, number: 42, name: "Evie Bagnell", grade: 7 },
+  { id: 5, number: 8, name: "Charlotte Hogge", grade: 7 },
+  { id: 6, number: 0, name: "Pearl Barry", grade: 7 },
+  { id: 7, number: 2, name: "Devan Bryant", grade: 7 },
+  { id: 8, number: 24, name: "Ellie Montgomery", grade: 7 },
+  { id: 9, number: 43, name: "Harper Roberts", grade: 7 },
+  { id: 10, number: 0, name: "Zinn Scalin", grade: 7 },
+  { id: 11, number: 17, name: "Louise Totton", grade: 7 },
+  { id: 12, number: 23, name: "Mary Willey", grade: 7 },
+  { id: 13, number: 34, name: "Jaidah Williams", grade: 7 },
 ];
 
 function PlayerCard({ player }: { player: Player }) {
